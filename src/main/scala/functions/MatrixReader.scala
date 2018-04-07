7b159168a0a7a0c6087bc3f4c1102fbbadb0e9b2
robertbutacu <robert.butacu@yahoo.com>
2018-04-07 12:44:48 +0300

    more scaffolding

--- src/main/scala/functions/MatrixReader.scala
+++ src/main/scala/functions/MatrixReader.scala
@@ -7,38 +7,40 @@ import data.{RowValue, SparseMatrix}
 import scala.annotation.tailrec
 import scala.io.Source
 
-trait MatrixReader {
-  def readFromFile[F: Fractional](filename: String): SparseMatrix[F]
+trait MatrixReader[F] {
+  def readFromFile(filename: String): SparseMatrix[F]
 }
 
 object MatrixReader {
-  implicit def sparseMatrixReader: MatrixReader = new MatrixReader {
-    override def readFromFile[F: Fractional](filename: String): SparseMatrix[F] = {
-      def readVector(lines: Iterator[String]): List[Double] = {
-        List.empty
-      }
-
-      def readMatrix(lines: Iterator[String], toParse: Int): List[RowValue[F]] = {
-        @tailrec
-        def go(lines: Iterator[String], toParse: Int, matrixLines: List[RowValue[F]]): List[RowValue[F]] = {
-          if (toParse == 0)
-            matrixLines
-          else {
-            val currLine = lines.take(1)
-
-            go(lines, toParse - 1, matrixLines)
-          }
-        }
-
-        go(lines, toParse, List.empty)
-      }
-
-      val lines = Source.fromFile(filename).getLines()
-
-      val numberOfLines = lines.take(1)
-
-      println(numberOfLines.toList.head.toInt)
-      SparseMatrix(List.empty)
+  implicit def sparseMatrixReader: MatrixReader[Double] = (filename: String) => {
+    def readVector(lines: Iterator[String]): List[Double] = {
+      List.empty
     }
+
+    def readMatrix(lines: Iterator[String], toParse: Int): List[RowValue[Double]] = {
+      @tailrec
+      def go(lines: Iterator[String], toParse: Int, matrixLines: List[RowValue[Double]]): List[RowValue[Double]] = {
+        if (toParse == 0)
+          matrixLines
+        else {
+          val currLine = lines.take(1)
+
+          val rowValues = currLine.toList.head.split(",").map(_.toDouble)
+
+          val rowValue = RowValue(rowValues(0), rowValues(1).toInt, rowValues(2).toInt)
+
+          go(lines, toParse - 1, matrixLines :+ rowValue)
+        }
+      }
+
+      go(lines, toParse, List.empty)
+    }
+
+    val lines = Source.fromFile(filename).getLines()
+
+    val numberOfLines = lines.take(1)
+
+    println(numberOfLines.toList.head.toInt)
+    SparseMatrix(List.empty)
   }
 }
