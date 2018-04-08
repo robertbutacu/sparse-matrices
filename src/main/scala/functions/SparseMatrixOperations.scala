9f13a7d1c6b9311beb09f4f0569d765ac4e82a65
robertbutacu <robert.butacu@yahoo.com>
2018-04-08 19:56:38 +0300

    will prolly use iterators for efficiency

--- src/main/scala/functions/SparseMatrixOperations.scala
+++ src/main/scala/functions/SparseMatrixOperations.scala
@@ -15,6 +15,9 @@ object SparseMatrixOperations {
       val nrOfColumns = Math.max(A.maxByColumn, B.maxByColumn)
       val nrOfRows = Math.max(A.rows.length, B.rows.length)
 
+      val firstMatrixIterator = A.rows.toIterator
+      val secondMatrixIterator = B.rows.toIterator
+
       SparseMatrix(List.empty)
 
     }
