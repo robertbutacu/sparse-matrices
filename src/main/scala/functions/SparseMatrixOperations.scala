f65ce539f153f5a82b3d4b0f1e45726106b7e59e
robertbutacu <robert.butacu@yahoo.com>
2018-04-08 20:05:33 +0300

    scaffolding

--- src/main/scala/functions/SparseMatrixOperations.scala
+++ src/main/scala/functions/SparseMatrixOperations.scala
@@ -1,6 +1,6 @@
 package functions
 
-import data.SparseMatrix
+import data.{Row, RowValue, SparseMatrix}
 
 trait SparseMatrixOperations {
   def ***[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F]
@@ -8,15 +8,26 @@ trait SparseMatrixOperations {
 }
 
 object SparseMatrixOperations {
+  case class IteratorRow[F: Fractional](index: Int, values: Iterator[RowValue[F]])
+  case class ConcurrentIterator[F: Fractional](first: Iterator[RowValue[F]], second: Iterator[RowValue[F]])
+
+  type RowParser[F: Fractional] = Iterator[IteratorRow[F]]
+
   def sparseMatrixOperations: SparseMatrixOperations = new SparseMatrixOperations {
     override def ***[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F] = ???
 
     override def +++[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F] = {
+      def addMatrices(firstMatrix: RowParser[F], secondMatrix: RowParser[F], currentIterator: ConcurrentIterator[F],
+                      currRow: Int, currColumn: Int): SparseMatrix[F] = {
+        SparseMatrix(List.empty)
+      }
+
+
       val nrOfColumns = Math.max(A.maxByColumn, B.maxByColumn)
       val nrOfRows = Math.max(A.rows.length, B.rows.length)
 
-      val firstMatrixIterator = A.rows.map(r => r.values.toIterator).toIterator
-      val secondMatrixIterator = B.rows.map(r => r.values.toIterator).toIterator
+      val firstMatrixIterator = A.rows.map(r => IteratorRow(r.index, r.values.toIterator)).toIterator
+      val secondMatrixIterator = B.rows.map(r =>IteratorRow(r.index, r.values.toIterator)).toIterator
 
       //the idea would be the following:
       /*
@@ -29,6 +40,8 @@ object SparseMatrixOperations {
       6. curr column
 
       The idea would be the following:
+      1. pull a new row when the curr column has run out of elements
+
        */
       SparseMatrix(List.empty)
 
