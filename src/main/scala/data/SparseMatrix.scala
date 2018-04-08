d97841a4ca387a18af9f74e9b93dc3d69d53052a
robertbutacu <robert.butacu@yahoo.com>
2018-04-08 19:55:31 +0300

    scaffolding of addition

--- src/main/scala/data/SparseMatrix.scala
+++ src/main/scala/data/SparseMatrix.scala
@@ -2,4 +2,6 @@ package data
 
 case class SparseMatrix[F: Fractional](rows: List[Row[F]]) {
   require(rows.forall(r => r.values.length <= 10))
+
+  val maxByColumn = rows.maxBy(_.values.length).values.length
 }
--- src/main/scala/functions/SparseMatrixOperations.scala
+++ src/main/scala/functions/SparseMatrixOperations.scala
@@ -11,6 +11,12 @@ object SparseMatrixOperations {
   def sparseMatrixOperations: SparseMatrixOperations = new SparseMatrixOperations {
     override def ***[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F] = ???
 
-    override def +++[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F] = ???
+    override def +++[F: Fractional](A: SparseMatrix[F], B: SparseMatrix[F]): SparseMatrix[F] = {
+      val nrOfColumns = Math.max(A.maxByColumn, B.maxByColumn)
+      val nrOfRows = Math.max(A.rows.length, B.rows.length)
+
+      SparseMatrix(List.empty)
+
+    }
   }
 }
