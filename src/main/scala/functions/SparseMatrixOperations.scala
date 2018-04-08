d2ec6f4e1eb93d3052d6db9ee69ca306901a61e4
robertbutacu <robert.butacu@yahoo.com>
2018-04-08 19:57:53 +0300

    explanatory comment

--- src/main/scala/functions/SparseMatrixOperations.scala
+++ src/main/scala/functions/SparseMatrixOperations.scala
@@ -18,6 +18,16 @@ object SparseMatrixOperations {
       val firstMatrixIterator = A.rows.toIterator
       val secondMatrixIterator = B.rows.toIterator
 
+      //the idea would be the following:
+      /*
+      have a function with the parameters:
+      1. firstMatrixIterator
+      2. secondMatrixIterator
+      3. firstMatrixIterator head buffer
+      4. secondMatrixIterator head buffer
+      5. curr row
+      6. curr column
+       */
       SparseMatrix(List.empty)
 
     }
