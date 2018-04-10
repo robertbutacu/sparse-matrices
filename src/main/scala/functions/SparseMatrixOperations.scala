f026b79a28ca8484a8d4d906f4eecda38747b904
robertbutacu <robert.butacu@yahoo.com>
2018-04-10 09:51:18 +0300

    changed function signature

--- src/main/scala/functions/SparseMatrixOperations.scala
+++ src/main/scala/functions/SparseMatrixOperations.scala
@@ -24,7 +24,7 @@ object SparseMatrixOperations {
 
   def sparseMatrixOperations: SparseMatrixOperations[SparseMatrix, Double] = new SparseMatrixOperations[SparseMatrix, Double] {
     override def ***(A: SparseMatrix[Double], B: SparseMatrix[Double]): SparseMatrix[Double] = {
-      def multiplyColumnWithRow[F: Fractional](row: Row[F], columns: List[ColumnValue[F]]): F = {
+      def multiplyColumnWithRow[F: Fractional](row: List[RowValue[F]], columns: List[ColumnValue[F]]): F = {
         implicitly[Fractional[F]].zero
       }
 
@@ -32,7 +32,7 @@ object SparseMatrixOperations {
              secondColumns: List[Column[Double]],
              result: List[Row[Double]]): List[Row[Double]] = {
           val newResult = firstRows.map{e =>
-            Row(e.index, secondColumns.map(c => RowValue[Double](c.index, multiplyColumnWithRow(e, c.values))) )
+            Row(e.index, secondColumns.map(c => RowValue[Double](c.index, multiplyColumnWithRow(e.values, c.values))) )
           }
           newResult
       }
