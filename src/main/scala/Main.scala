30a995e48a8761c9e58679e4eff90f9da5a44706
robertbutacu <robert.butacu@yahoo.com>
2018-04-10 10:30:09 +0300

    timers for functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,21 @@
+import java.util.Calendar
+
 import data.{AdditionResult, MultiplicationResult, Simple, SparseMatrix}
 import functions.MatrixReader.sparseMatrixReader
 import functions.SparseMatrixOperations.sparseMatrixOperations
 
 object Main extends App {
+
+  def printCurrentTime(): String = {
+    val now = Calendar.getInstance()
+    val currentHour = now.get(Calendar.HOUR)
+    val currentMinute = now.get(Calendar.MINUTE)
+    val currentSecond = now.get(Calendar.SECOND)
+    val currentMilli = now.get(Calendar.MILLISECOND)
+
+    s"""$currentHour : $currentMinute : $currentSecond :: $currentMilli"""
+  }
+  println(s"""Starting reading resources ${printCurrentTime()}""")
   val aPath = "E:\\projects\\sparse-matrices\\resources\\a.txt"
   val bPath = "E:\\projects\\sparse-matrices\\resources\\b.txt"
   val aplusbPath = "E:\\projects\\sparse-matrices\\resources\\aplusb.txt"
@@ -13,14 +26,17 @@ object Main extends App {
   val aplusbExpected = sparseMatrixReader.readFromFile(aplusbPath, isWithVector = true, AdditionResult)
   val atimesbExpected = sparseMatrixReader.readFromFile(atimesbPath, isWithVector = true, MultiplicationResult)
 
+  //println(s"""Starting adding matrices ${printCurrentTime()}""")
   //val aplusbActual = sparseMatrixOperations.+++(a.matrix, b.matrix)
 
- // println(SparseMatrix.equals(aplusbActual, aplusbExpected.matrix))
-  //a.matrix.asColumns.foreach(println)
+  //println(s"""Finished adding ${printCurrentTime()}""")
+ //println("Checking equality")
+  // println(SparseMatrix.equals(aplusbActual, aplusbExpected.matrix))
 
+  println(s"""Multiplying matrices ${printCurrentTime()}""")
   val atimesbActual = sparseMatrixOperations.***(a.matrix, b.matrix)
 
-  //atimesbActual.rows.foreach(println)
-  //atimesbExpected.matrix.rows.foreach(println)
+  println(s"""Finished multiplying ${printCurrentTime()}""")
+  println(s"""Checking equality ${printCurrentTime()}""")
   println(SparseMatrix.equals(atimesbActual, atimesbExpected.matrix))
 }
