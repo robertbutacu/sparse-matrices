13d4958c55b46171842a02190f84a9eba7347757
robertbutacu <robert.butacu@yahoo.com>
2018-04-18 13:53:22 +0300

    should be donezo - i think

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,12 +7,10 @@ import functions.matrix.operations.liniar.system.gauss.seidel.{LinearSystemSolve
 object Main extends App {
   //LabRunner.runLab4()
   val matrix = SparseMatrix(List(
-    Row(0, List(RowValue(0, 102.5))),
-    Row(1, List(RowValue(0, 3.5), RowValue(1, 104.88), RowValue(2, 1.05), RowValue(4, 0.33))),
-    Row(2, List(RowValue(2, 100.0))),
-    Row(3, List(RowValue(1, 1.3), RowValue(3, 101.3))),
-    Row(4, List(RowValue(0, 0.73), RowValue(3, 1.5), RowValue(4, 102.23)))
+    Row(0, List(RowValue(0, 4.0), RowValue(1, -1.0), RowValue(2, -1.0))),
+    Row(1, List(RowValue(0, -2.0), RowValue(1, 6.0), RowValue(2, 1.0))),
+    Row(2, List(RowValue(0, -1.0), RowValue(1, 1.0), RowValue(2, 7.0)))
   ))
   //LabRunner.runLab5()
-  LinearSystemSolver.solve(MatrixWithVector(matrix, Some(List(6.0, 7.0, 8.0, 9.0, 1.0))), Precision(5))
+  LinearSystemSolver.solve(MatrixWithVector(matrix, Some(List(3.0, 9.0, -6.0))), Precision(5))
 }
