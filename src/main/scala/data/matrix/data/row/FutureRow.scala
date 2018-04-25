1d5de30fcf0c74bc5e32ab31b864137477eb6038
robertbutacu <robert.butacu@yahoo.com>
2018-04-25 15:44:25 +0300

    removed useless files

--- src/main/scala/data/matrix/data/row/FutureRow.scala
+++
@@ -1,8 +0,0 @@
-package data.matrix.data.row
-
-import scala.concurrent.Future
-
-case class FutureRow[F: Fractional](index: Int, values: List[Future[RowValue[F]]] = List.empty){
-  //require(values.last.columnIndex == index)
-}
-
